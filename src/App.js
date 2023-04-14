1a6b4a6a96368e17587b0bbbaeb6b2a29b17df22
alexyoliveira <cupofteaoficial@gmail.com>
2023-04-14 11:43:54 -0300

    initial page created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getCategories } from './services/api';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
+import InitialPage from './pages/InitialPage';
 
 function App() {
   return (
-    <div className="App">
-      {
-        getCategories()
-      }
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ InitialPage } />
+    </Switch>
   );
 }
 
---
+++ src/pages/InitialPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InitialPage() {
+  return (
+    <div data-testid="home-initial-message">
+      Digite algum termo de pesquisa ou escolha uma categoria.
+    </div>
+  );
+}
+
+export default InitialPage;
