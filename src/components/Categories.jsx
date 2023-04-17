76f903a4c29af39a12f7aab02304b1fdcf7721d8
alexyoliveira <cupofteaoficial@gmail.com>
2023-04-17 20:22:57 -0300

    requirement 06 done, search filter by category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { getCategories } from '../services/api';
+import { useDispatch } from 'react-redux';
+import {
+  getCategories,
+  getProductsFromCategoryAndQuery,
+} from '../services/api';
+import { saveProducts } from '../redux/actions';
 
 function Categories() {
   const [categories, setCategories] = useState([]);
+  const dispatch = useDispatch();
   useEffect(() => {
     const allCategories = async () => {
       const categoriesReturn = await getCategories();
@@ -10,16 +16,24 @@ function Categories() {
     };
     allCategories();
   }, []);
+
+  const handleClick = async (id) => {
+    const products = await getProductsFromCategoryAndQuery(id, '');
+    dispatch(saveProducts(products.results));
+  };
   return (
     <div>
       <ul>
         {categories.map((categorie) => (
-          <li data-testid="category" key={ categorie.id }>
+          <li
+            onClick={ () => handleClick(categorie.id) }
+            data-testid="category"
+            key={ categorie.id }
+          >
             {categorie.name}
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
